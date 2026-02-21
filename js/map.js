2ffb1481641edaae32c08788d04af4a0e9d9f40c
dripnooblovesyou <awesomepants980@gmail.com>
2026-02-21 10:50:02 -0800

    set up leaflet and mapbox

--- js/map.js
+++ js/map.js
@@ -1,1 +1,11 @@
+// Default map
+var map = L.map('map').setView([47.6, -122.3], 12);
 
+// Mapbox tiles
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=YOUR_TOKEN', {
+  attribution: 'Map data &copy; OpenStreetMap contributors',
+  id: 'mapbox/streets-v11',
+  tileSize: 512,
+  zoomOffset: -1,
+  accessToken: 'YOUR_TOKEN'
+}).addTo(map);
